992f164e00e2ddbeb8ae4f5561fb50ec79730ed2
Kris Latham <kris.latham@gmail.com>
2026-01-14 21:33:33 -0600

    Testing

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color=#BF5700 icon="\uf005" block="Texas Icons"
 namespace texasIcons {
 
-    //% block="Show Cowboy Hat Icon with Delay"
+    //% block="Show Cowboy Hat Icon with Delay %delay ms"
     //% delay.defl=150
     //% delay.min=20 delay.max=1000
     export function showCowboyHatIcon(delay: number = 150) {
@@ -56,11 +56,7 @@ namespace texasIcons {
 # # # # #`)
         ]
         
-        //scrollFrames(frames, delay)
-        for (let frame of frames) {
-            frame.showImage(0)
-            basic.pause(delay)
-        }
+        scrollFrames(frames, delay)
     }
 
     //% blockHidden=true
